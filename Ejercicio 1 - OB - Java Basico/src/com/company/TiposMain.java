90ea606f407469e00e5bcfc1dcf861f83135281f
RubenEzengaCarelli <102779524+RubenEzengaCarelli@users.noreply.github.com>
2022-10-22 16:08:03 -0300

    Add files via upload

---
+++ Ejercicio 1 - OB - Java Basico/src/com/company/TiposMain.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class TiposMain {
+
+    public static void main(String[] args) {
+
+        // ENTEROS
+        byte num1 = 1;
+        short num2 = 2;
+        int num3 = 3;
+        long num4 = 4;
+
+        // FLOTANTE
+        float dec1 = 1.3f;
+        double dec2 = 1.44d;
+
+        //CARACTER
+        char char1 = 'a';
+
+        // BOOLEANOS
+        boolean verdadero = true;
+        boolean falso = false;
+
+        // CADENAS DE TEXTO
+        String nombre = "Rubén";
+        String apellido = "Carelli";
+
+        // TIPOS ENVOLTORIO
+        Integer numEnv1 = null;
+        Long numEnv2 = 2L; //
+
+
+    }
+}
